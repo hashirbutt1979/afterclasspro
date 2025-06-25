20ae4bed00e95971704017232fd6924fce9b8bb8
hashirbutt1979 <ndakhurram@gmail.com>
2025-06-25 22:41:56 +0500

    lesson21isdone

---
+++ lesson21/demo.js
@@ -0,0 +1,26 @@
+var hiba = "hey there"
+
+console.log(hiba.toUpperCase())
+console.log(hiba.toLowerCase())
+console.log(hiba.indexOf(h))
+
+let i
+for (i = 1; i <= 500; i++)
+    console.log("kabadi" + i)
+
+var day = 3
+switch (day) {
+    case 1:
+        console.log("monday")
+    case 2:
+        console.log("tuesday")
+    case 3:
+        console.log("wednesday")
+        break;
+    case 4:
+        console.log("thursday")
+
+}
+
+
+
